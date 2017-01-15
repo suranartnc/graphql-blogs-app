6b5e2e92014414f9d1e4b49d3ee75dd042707464
Suranart Niamcome <suranartnc@gmail.com>
2017-01-15 11:56:47 +0700

    add write page

---
+++ src/app/pages/WritePage.js
@@ -0,0 +1,17 @@
+import React, {Component, PropTypes} from 'react'
+
+class WritePage extends Component {
+  render () {
+    return (
+      <div>
+        Write
+      </div>
+    )
+  }
+}
+
+WritePage.propTypes = {
+
+}
+
+export default WritePage
