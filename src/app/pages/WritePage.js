6e65c771f727f93625af6f800912273503b1d1f3
Suranart Niamcome <suranartnc@gmail.com>
2017-01-15 12:03:15 +0700

    add write form

--- src/app/pages/WritePage.js
+++ src/app/pages/WritePage.js
@@ -1,10 +1,31 @@
 import React, {Component, PropTypes} from 'react'
 
 class WritePage extends Component {
+
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleInputChange = (field) => (e) => {
+    this.setState({
+      [field]: e.target.value
+    })
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
   render () {
     return (
       <div>
-        Write
+        <form onSubmit={this.handleFormSubmit}>
+          <input type="text" onChange={this.handleInputChange('title')} />
+          <textarea onChange={this.handleInputChange('body')} />
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
