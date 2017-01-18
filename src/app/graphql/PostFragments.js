f0a35b3a59c43255d31554c8062cea9aba091cad
Suranart Niamcome <suranartnc@gmail.com>
2017-01-18 23:20:14 +0700

    add instructions

--- src/app/graphql/PostFragments.js
+++ src/app/graphql/PostFragments.js
@@ -1,5 +1,11 @@
 import gql from 'graphql-tag'
 
+// There are two principal uses for fragments in Apollo:
+
+// 1. Sharing fields between multiple queries, mutations or subscriptions.
+// 2. Breaking your queries up to allow you to co-locate field access with the places they are used.
+// (split some subfields of a field in to a group to pass to sub components that actually use those fields => easy to validate + filter props)
+
 export const MainFields = gql`
   fragment MainFields on PostType {
     _id
