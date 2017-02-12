bb4eda1f4095a46a6ff9ce4f01ccaf2721f6d007
SuranartN <suranartnc@gmail.com>
2017-02-12 17:48:44 +0700

    improve config for file-loader

--- .noxt/webpack/dev.js
+++ .noxt/webpack/dev.js
@@ -32,7 +32,10 @@ module.exports = function(env) {
         {
           test: /\.(jpe?g|png|gif|svg)$/i,
           exclude: /node_modules/,
-          loader: 'file-loader'
+          loader: 'file-loader',
+          query: {
+            name: env === 'dev' ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
+          },
         }
       ]
     },
--- .noxt/webpack/prod.js
+++ .noxt/webpack/prod.js
@@ -62,7 +62,12 @@ module.exports = function(env) {
           test: /\.(jpe?g|png|gif|svg)$/i,
           exclude: /node_modules/,
           loaders: [
-            'file-loader',
+            {
+              loader: 'file-loader',
+              query: {
+                name: env === 'dev' ? '[path][name].[ext]?[hash:8]' : '[hash:8].[ext]',
+              }
+            },
             {
               loader: 'image-webpack-loader',
               query: {
@@ -95,7 +100,17 @@ module.exports = function(env) {
       }),
       new webpack.optimize.UglifyJsPlugin({
         compressor: {
+          screw_ie8: true, // React doesn't support IE8
           warnings: false,
+          unused: true,
+          dead_code: true,
+        },
+        mangle: {
+          screw_ie8: true,
+        },
+        output: {
+          comments: false,
+          screw_ie8: true,
         },
       }),
       new AssetsPlugin({
@@ -121,6 +136,12 @@ module.exports = function(env) {
         manifest: require('../../static/build/react-manifest.json'),
       }),
       new ProgressBarPlugin(),
-    ]
+    ],
+
+    node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+    }
   })
 }
