8c2a333f524db1856ba5cb1c2d8371fe44f0f88a
Thongchai Kitiyanantawong <mypumz@gmail.com>
2017-02-14 12:12:19 +0700

    Add simple story with image and Scss file

---
+++ stories/Styleguide.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from 'styles/storybook/styleguide.scss'
+import logo from 'static/images/react.png'
+
+const componentName = () => {
+  return (
+    <div className={styles.container}>
+      <img src={logo} />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+  )
+}
+
+export default componentName
--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 
-import Styleguide from 'components/Styleguide'
+import Styleguide from './Styleguide'
+
+const CenterDecorator = (story) => (
+  <div style={{
+    textAlign: 'center',
+    width: '350px',
+    margin: 'auto',
+    backgroundColor: '#eee',
+    padding: '30px'
+  }}>
+    {story()}
+  </div>
+)
 
 storiesOf('Styleguide', module)
+  .addDecorator(CenterDecorator)
   .add('to Storybook', () => (
     <Styleguide />
   ))
