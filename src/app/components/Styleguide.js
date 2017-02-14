9765401490224d19c4181b119c7efac58acb5606
Thongchai Kitiyanantawong <mypumz@gmail.com>
2017-02-14 12:01:04 +0700

    Add file loader for Storybook Webpack config

--- src/app/components/Styleguide.js
+++ src/app/components/Styleguide.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react'
 
 import styles from 'styles/storybook/styleguide.scss'
+import logo from 'static/images/react.png'
 
 class Styleguide extends Component {
   render () {
     return (
       <div className={styles.container}>
-        Test
+        <img src={logo} style={{ width: '300px', height: '300px' }} />
       </div>
     )
   }
