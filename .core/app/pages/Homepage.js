6b18d0b52f07a1d0a483b391f73ebd81d6c2b84a
Suranart Niamcome <suranartnc@gmail.com>
2017-02-14 12:07:24 +0700

    edit global title

--- .core/app/pages/Homepage.js
+++ .core/app/pages/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Homepage () {
-  return <div>core.js</div>
+  return <div>Homepage</div>
 }
 
 export default Homepage
--- src/app/routes.js
+++ src/app/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
-import PageLayout from 'app/pages/layouts/PageLayout'
+import PageLayout from 'pages/layouts/PageLayout'
 
-import HomePage from 'app/pages/HomePage'
-import AboutPage from 'app/pages/AboutPage'
-import WritePage from 'app/pages/WritePage'
-import EntryPage from 'app/pages/EntryPage'
+import HomePage from 'pages/HomePage'
+import AboutPage from 'pages/AboutPage'
+import WritePage from 'pages/WritePage'
+import EntryPage from 'pages/EntryPage'
 
 import ErrorPage from 'core/app/pages/ErrorPage'
 
