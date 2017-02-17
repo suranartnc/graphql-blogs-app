1186abc03f30d6b65ee19dd3086c19d9629a2d40
Thongchai Kitiyanantawong <mypumz@gmail.com>
2017-02-17 17:48:57 +0700

    handle post date

--- src/app/components/PostItem.js
+++ src/app/components/PostItem.js
@@ -1,20 +1,31 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
+import moment from 'moment'
+import 'moment/locale/th'
+
 import styles from 'styles/components/PostItem.scss'
+
+const renderDate = date => {
+  return moment(date).fromNow()
+}
 
 function PostItem ({ post }) {
   return (
-    <article key={post._id}>
+    <article key={post._id} className={styles.container}>
       <div className={styles.thumbnail}>
-        <img src={post.img} alt={post.title} />
+        <Link to={`/post/${post._id}`}>
+          <img src={post.img} alt={post.title} />
+        </Link>
       </div>
-      <div className={styles.detail}>
-        <h2 className={styles.title}><Link to={`/post/${post._id}`}>{post.title}</Link></h2>
-        <p className={styles.desc}>{post.desc}</p>
-      </div>
-      <div className={styles.meta}>
-        <Link className={styles.category} to={post.category_link} >{post.category}</Link>
-        <time className={styles.date}>{post.date}</time>
+      <div className={styles.content}>
+        <div className={styles.detail}>
+          <h2 className={styles.title}><Link to={`/post/${post._id}`}>{post.title}</Link></h2>
+          <p className={styles.desc}>{post.desc}</p>
+        </div>
+        <div className={styles.meta}>
+          <Link className={styles.category} to={post.category_link} >{post.category}</Link>
+          <time className={styles.date}>{renderDate(post.date)}</time>
+        </div>
       </div>
     </article>
   )
