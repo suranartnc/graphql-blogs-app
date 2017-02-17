7379417530dbd2f8595c59eb85aa896cbe56ceba
Thongchai Kitiyanantawong <mypumz@gmail.com>
2017-02-17 16:16:40 +0700

    Add global style to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
 import { configure } from '@kadira/storybook'
 
+import 'styles/global/app.scss'
+import 'styles/storybook/base.scss'
+
 function loadStories () {
   require('../stories')
   require('../stories/PostListStory.js')
