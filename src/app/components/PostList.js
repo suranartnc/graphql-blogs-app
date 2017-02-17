63871ca09fe5a4e8661a50106f1f83a1a183f671
Thongchai Kitiyanantawong <mypumz@gmail.com>
2017-02-17 15:14:38 +0700

    Fix conflict file

--- src/app/components/PostList.js
+++ src/app/components/PostList.js
@@ -5,25 +5,7 @@ import PostItem from 'components/PostItem'
 function PostList ({ data: { posts } = { posts: [] } }) {
   return (
     <div>
-<<<<<<< HEAD
-      {posts.map(post => (
-        <article key={post._id}>
-          <div className={styles.thumbnail}>
-            <img src={post.img} alt={post.title} />
-          </div>
-          <div className={styles.detail}>
-            <h2 className={styles.title}><Link to={`/post/${post._id}`}>{post.title}</Link></h2>
-            <p className={styles.desc}>{post.desc}</p>
-          </div>
-          <div className={styles.meta}>
-            <Link className={styles.category} to={post.category_link} >{post.category}</Link>
-            <time className={styles.date}>{post.date}</time>
-          </div>
-        </article>
-      ))}
-=======
       {posts.map(post => <PostItem key={post._id} post={post} />)}
->>>>>>> origin/master
     </div>
   )
 }
