83fe07f1cb4eadcb025010fe84e9699de7dfb3d7
Suranart Niamcome <suranartnc@gmail.com>
2017-02-21 14:38:58 +0700

    remove withCache

--- src/app/pages/HomePage.js
+++ src/app/pages/HomePage.js
@@ -5,7 +5,6 @@ import { pure, withHandlers, compose } from 'recompose'
 
 import { GET_POSTS } from 'app/modules/post/graphql/postQueries'
 import { fetchMore as fetchMoreUtil } from 'utils/apollo'
-import withCache from 'hocs/withCache'
 import withPreloader from 'hocs/withPreloader'
 import PostList from 'components/PostList'
 import logo from 'static/images/react.png'
@@ -39,7 +38,6 @@ HomePage.propTypes = {
 }
 
 export default compose(
-  withCache,
   graphql(GET_POSTS, {
     options: {
       forceFetch: true,
