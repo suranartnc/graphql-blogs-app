8d07ff712744fb7ba7473c276fe1bf7f163db706
Suranart Niamcome <suranartnc@gmail.com>
2017-02-22 20:04:19 +0700

    make code splitting works in server bundle

--- src/app/routes.js
+++ src/app/routes.js
@@ -5,32 +5,25 @@ import PageLayout from 'pages/layouts/PageLayout'
 
 import HomePage from 'pages/HomePage'
 import AboutPage from 'pages/AboutPage'
-import GridPage from 'pages/GridPage'
 import WritePage from 'pages/WritePage'
 import EntryPage from 'pages/EntryPage'
-
 import ErrorPage from 'core/app/pages/ErrorPage'
-
-// if(typeof require.ensure !== 'function') {
-//   require.ensure = function requireModule (deps, callback) {
-//     callback(require)
-//   }
-// }
 
 export default function getRoutes () {
   return (
     <Route component={PageLayout}>
       <IndexRoute component={HomePage} />
       <Route path="/about" component={AboutPage} />
-      <Route path="/grid" component={GridPage} />
-      
-      {/*<Route path="/grid"
+      <Route path="/grid"
         getComponent={(nextState, cb) => {
+          if (process.env.BROWSER != true) {
+            return cb(null, require('./pages/GridPage').default);
+          }
           require.ensure([], require => {
             cb(null, require('./pages/GridPage').default);
           }, 'grid');
         }}
-      />*/}
+      />
       <Route path="/write" component={WritePage} />
       <Route path="/post/:id" component={EntryPage} />
       <Route path="*" component={ErrorPage} status="404" />
