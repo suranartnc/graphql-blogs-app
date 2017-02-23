c9488443ef117369ce991ce633d32fb26c28313e
Suranart Niamcome <suranartnc@gmail.com>
2017-02-23 18:34:53 +0700

    ignore require.ensure for server-side

--- src/app/routes.js
+++ src/app/routes.js
@@ -16,9 +16,6 @@ export default function getRoutes () {
       <Route path="/about" component={AboutPage} />
       <Route path="/grid"
         getComponent={(nextState, cb) => {
-          if (process.env.BROWSER !== true) {
-            return cb(null, require('./pages/GridPage').default)
-          }
           require.ensure([], require => {
             cb(null, require('./pages/GridPage').default)
           }, 'grid')
