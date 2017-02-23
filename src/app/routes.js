f53df8595a542f431bde5975c780092f4b824772
Suranart Niamcome <suranartnc@gmail.com>
2017-02-23 18:45:02 +0700

    apply code splitting to all pages

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,17 +3,23 @@ import { Route, IndexRoute } from 'react-router'
 
 import PageLayout from 'pages/layouts/PageLayout'
 
-import HomePage from 'pages/HomePage'
-import AboutPage from 'pages/AboutPage'
-import WritePage from 'pages/WritePage'
-import EntryPage from 'pages/EntryPage'
-import ErrorPage from 'core/app/pages/ErrorPage'
-
 export default function getRoutes () {
   return (
     <Route component={PageLayout}>
-      <IndexRoute component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <IndexRoute
+        getComponent={(nextState, cb) => {
+          require.ensure([], require => {
+            cb(null, require('./pages/HomePage').default)
+          }, 'home')
+        }}
+      />
+      <Route path="/about"
+        getComponent={(nextState, cb) => {
+          require.ensure([], require => {
+            cb(null, require('./pages/AboutPage').default)
+          }, 'about')
+        }}
+      />
       <Route path="/grid"
         getComponent={(nextState, cb) => {
           require.ensure([], require => {
@@ -21,9 +27,28 @@ export default function getRoutes () {
           }, 'grid')
         }}
       />
-      <Route path="/write" component={WritePage} />
-      <Route path="/post/:id" component={EntryPage} />
-      <Route path="*" component={ErrorPage} status="404" />
+      <Route path="/write"
+        getComponent={(nextState, cb) => {
+          require.ensure([], require => {
+            cb(null, require('./pages/WritePage').default)
+          }, 'write')
+        }}
+      />
+      <Route path="/post/:id"
+        getComponent={(nextState, cb) => {
+          require.ensure([], require => {
+            cb(null, require('./pages/EntryPage').default)
+          }, 'entry')
+        }}
+      />
+      <Route path="*"
+        status="404"
+        getComponent={(nextState, cb) => {
+          require.ensure([], require => {
+            cb(null, require('../../.core/app/pages/ErrorPage').default)
+          }, 'error')
+        }}
+      />
     </Route>
   )
 }
