899d26eaa83c04efad157710507a9af90d6c0244
Suranart Niamcome <suranartnc@gmail.com>
2017-02-23 14:01:29 +0700

    fix lint errors

--- src/app/routes.js
+++ src/app/routes.js
@@ -16,12 +16,12 @@ export default function getRoutes () {
       <Route path="/about" component={AboutPage} />
       <Route path="/grid"
         getComponent={(nextState, cb) => {
-          if (process.env.BROWSER != true) {
-            return cb(null, require('./pages/GridPage').default);
+          if (process.env.BROWSER !== true) {
+            return cb(null, require('./pages/GridPage').default)
           }
           require.ensure([], require => {
-            cb(null, require('./pages/GridPage').default);
-          }, 'grid');
+            cb(null, require('./pages/GridPage').default)
+          }, 'grid')
         }}
       />
       <Route path="/write" component={WritePage} />
