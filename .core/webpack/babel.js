edf1156589dcb643f85452d799a93bd0b48ae849
Suranart Niamcome <suranartnc@gmail.com>
2017-02-26 18:12:58 +0700

    resue configs

--- .core/webpack/babel.js
+++ .core/webpack/babel.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const getCSSOptions = require('./utils/getCSSOptions')
+const getImagesOptions = require('./utils/getImagesOptions')
 
 module.exports = function() {
   return {
@@ -10,50 +12,12 @@ module.exports = function() {
 
     module: {
       loaders: [
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-          ],
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                module: true,
-                importLoaders: 1,
-                sourceMap: true,
-                localIdentName: '[name]__[local]___[hash:base64:5]',
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-                includePaths: [
-                  path.join(process.cwd(), '.core/app/styles'),
-                  path.join(process.cwd(), 'src/app/styles')
-                ],
-              },
-            },
-          ],
-        },
+        ...getCSSOptions('development'),
+        ...getImagesOptions(process.env.NODE_ENV),
         {
           test: /\.(eot|svg|ttf|woff|woff2)$/,
           loader: 'file-loader',
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          exclude: /node_modules/,
-          loader: 'file-loader',
-          query: {
-            name: 'images/min/[name]-[hash:8].[ext]',
-          }
-        },
+        }
       ],
     },
   }
