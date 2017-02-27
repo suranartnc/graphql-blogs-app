2a0cf65b47d0808be92781f2500b98e5cbaf3f74
Thongchai Kitiyanantawong <mypumz@gmail.com>
2017-02-27 12:18:20 +0700

    Add images for test bundler

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,13 +3,14 @@ import {Link} from 'react-router'
 
 import Nav from 'components/Nav'
 import s from 'styles/components/Header.scss'
+import LogoImg from 'static/images/logo.svg'
 
 class Header extends Component {
   render () {
     return (
       <header className={s.container}>
         <div className={s.logo}>
-          <h1><Link to="/">Logo</Link></h1>
+          <h1><Link to="/"><img src={LogoImg} /></Link></h1>
         </div>
         <Nav />
         <div className={s.write}>
